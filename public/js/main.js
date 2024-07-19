875976dbb94a4c6691bb462ea3e06b40b0120373
shawnmendeez <158365647+shawnmendeez@users.noreply.github.com>
2024-07-18 23:34:44 -0400

    done

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,21 @@
+// Glide Carousel Library JS
 const config = {
     type: 'carousel',
     perView: 4
 };
 
 new Glide('.glide', config).mount()
+
+// Split-type logo JS
+const myText = new SplitType('#logo')
+
+//GSAP library logo JS
+gsap.to('.char', {
+    y: 0,
+    stagger: 0.05,
+    delay: 0.2,
+    duration: .1
+})
+
+// Animation on Scroll Library JS
+AOS.init();
